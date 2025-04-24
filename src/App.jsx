100c8468aa0f316ed039ddb980cdde75923c46cc
leommelo <lmmelo096@gmail.com>
2025-04-24 19:13:15 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Presentation from './Components/Presentation/Presentation'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Presentation/>
+      <div>teste</div>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/Components/Presentation/Presentation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Presentation.css'
+import { useState } from 'react'
+import LogoFuria from '../../assets/LogoFuria.svg'
+import Plantera from '../../assets/PLANTera1.svg'
+
+const Presentation = () => {
+  return (
+    <div className='presentation'>
+        <div className='presentation__logo'>
+            <img src={LogoFuria} alt="" />
+        </div>
+        <div className='info'>
+            <h2>Sua C4 de informações da FURIA.</h2>
+            <h1><span className='roxo'>PLANT</span>era</h1>
+            <h3>Conheça o novo chatbot oficial da Fúria: rápido e eficiente para te informar sobre os resultados das partidas e as próximas disputas do time de CS.</h3>
+            <img src={Plantera} alt="Imagem da PLANTera" className='plantera' />
+        </div>
+    </div>
+  )
+}
+
+export default Presentation
